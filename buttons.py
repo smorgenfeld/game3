2cfd24ba703df8f55e4ae967d7435967184bb936
smorgenfeld <smorgenfeld@users.noreply.github.com>
2015-12-15 09:43:43 -0800

    yay 3 button chjoice

--- buttons.py
+++ buttons.py
@@ -19,13 +19,8 @@ def button(p1x,p1y,p2x,p2y,text,win,undraw):
                     sleep(.2)
                     rekt.undraw()
                     lab.undraw()
-                    return
-                else:
-                    return
-            else:
-                lol=1
-        else:
-            lol = 2
+                return
+
 def buttonChoice(p1x,p1y,p2x,p2y,text1,text2,win,undraw):
     rekt1 = Rectangle(Point(p1x,p1y),Point((p1x+p2x)/2-((p1x+p2x)/40),p2y))
     rekt2 = Rectangle(Point((p1x+p2x)/2+((p1x+p2x)/40),p1y),Point(p2x,p2y))
@@ -42,11 +37,7 @@ def buttonChoice(p1x,p1y,p2x,p2y,text1,text2,win,undraw):
                 rekt1.setFill('white')
                 if undraw == True:
                     rekt1.undraw();rekt2.undraw();lab1.undraw();lab2.undraw()
-                    return 1
-                else:
-                    return 1
-            else:
-                lol=1
+                return 1
         elif x.getX() >= (p1x+p2x)/2+((p1x+p2x)/40) and x.getX() <= p2x:
             if x.getY() >= p1y and x.getY() <= p2y:
                 rekt2.setFill('grey')
@@ -54,11 +45,39 @@ def buttonChoice(p1x,p1y,p2x,p2y,text1,text2,win,undraw):
                 rekt2.setFill('white')
                 if undraw == True:
                     rekt1.undraw();rekt2.undraw();lab1.undraw();lab2.undraw()
-                    return 2
-                else:
-                    return 2
-            else:
-                lol=1
-        else:
-            lol = 3
-            lol = 2
+                return 2
+def buttonChoice3(p1x,p1y,p2x,p2y,text1,text2,text3,win,undraw):
+    rekt1 = Rectangle(Point(p1x,(p1y+p2y)/2+20),Point((p1x+p2x)/2-((p1x+p2x)/40),p2y))
+    rekt2 = Rectangle(Point((p1x+p2x)/2+((p1x+p2x)/40),(p1y+p2y)/2+20),Point(p2x,p2y))
+    rekt3 = Rectangle(Point(p1x+((p2x-p1x)/4),p1y),Point(p1x+(3*(p2x-p1x)/4),(p2y+p1y)/2-20))
+    rekt1.draw(win),rekt2.draw(win),rekt3.draw(win)
+    lab1 = Text(Point((rekt1.getCenter()).getX(),(rekt1.getCenter()).getY()),text1);lab1.draw(win)
+    lab2 = Text(Point((rekt2.getCenter()).getX(),(rekt2.getCenter()).getY()),text2);lab2.draw(win)
+    lab3 = Text(Point((rekt3.getCenter()).getX(),(rekt3.getCenter()).getY()),text3);lab3.draw(win)
+    i = False
+    while i == False:
+        x=win.getMouse()
+        if x.getX() >= p1x and x.getX() <= (p1x+p2x)/2-((p1x+p2x)/40):
+            if x.getY() >= (p1y+p2y)/2+20 and x.getY() <= p2y:
+                rekt1.setFill('grey')
+                sleep(.2)
+                rekt1.setFill('white')
+                if undraw == True:
+                    rekt1.undraw();rekt2.undraw();lab1.undraw();lab2.undraw()
+                return 1
+        elif x.getX() >= (p1x+p2x)/2+((p1x+p2x)/40) and x.getX() <= p2x:
+            if x.getY() >= (p1y+p2y)/2+20 and x.getY() <= p2y:
+                rekt2.setFill('grey')
+                sleep(.2)
+                rekt2.setFill('white')
+                if undraw == True:
+                    rekt1.undraw();rekt2.undraw();lab1.undraw();lab2.undraw()
+                return 2
+        elif x.getX() >= p1x+((p2x-p1x)/4) and x.getX() <= p1x+(3*(p2x-p1x)/4):
+            if x.getY() >= p1y and x.getY() <= (p2y+p1y)/2-20:
+                rekt3.setFill('grey')
+                sleep(.2)
+                rekt3.setFill('white')
+                if undraw == True:
+                    rekt1.undraw();rekt2.undraw();rekt3.undraw();lab1.undraw();lab2.undraw();lab3.undraw()
+                return 3
